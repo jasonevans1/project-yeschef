845f11c73c9ec542e4736c408c4d40af99fca848
Jason Evans <evans022@gmail.com>
2026-01-19 20:53:22 -0600

    Update playwright e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.BASE_URL || 'https://project-tabletop.ddev.site',
+    baseURL: process.env.BASE_URL || 'https://yeschef.ddev.site',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
